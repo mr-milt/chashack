d6f4a4f0a74f9af589744c2fab8777110ba6aec9
mr-milt <milton@marjasin.se>
2026-09-11 14:47:25 +0200

    3 side quests

--- src/pages/Button.jsx
+++ src/pages/Button.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 
 export default function Button() {
   const [text, setText] = useState("Klicka här!");
+  const [count, setCount] = useState(0)
+   const [hover, SetHover] = useState(false)
 
   const bytatext = () => {
     setText("Hej på dig! 👋");
   };
 
   return (
-    <div className="flex justify-center">
-      <div className="flex button bg-red-500 w-30 p-5 my-5 h-15 text-center items-center rounded-md">
+    <div className="flex gap-2 justify-around">
+
+      <div className="flex button bg-red-500 w-30 p-5 my-5 h-15 text-center items-center rounded-md hover:cursor-pointer">
         <button onClick={() => bytatext()}>{text}</button>
       </div>
+
+      <div className="flex button bg-red-500 w-30 p-5 my-5 h-15 text-center items-center rounded-md hover:cursor-pointer">
+        <button onClick={() => setCount(count + 1)}>Number: {count}</button>
+      </div>
+
+      <div className="flex relative button bg-red-500 w-30 p-5 my-5 h-15 text-center items-center rounded-md hover:cursor-pointer">
+        <div onMouseEnter={() => SetHover(true)} onMouseLeave={() => SetHover(false)}>{ hover ? <div className="absulute" > DU hovrar</div> : "Hover here"}</div>
+
+      </div>
+
+        { hover ? <div>Kolla vad göde sig här, hover vissar mig</div> : ""}
+
     </div>
   );
 }
