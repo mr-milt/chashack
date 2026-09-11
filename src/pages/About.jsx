caf63e0010059a5884dc7f8951936bfef0b71ea2
mr-milt <milton@marjasin.se>
2026-09-11 13:06:45 +0200

    rows

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,30 +2,38 @@ import react from "react";
 
 export default function About() {
   return (
-    <div className="flex flex-col justify-center text-red-500 gap-3 m-5">
-
-      <div className="flex justify-center text-3xl text-center items-center font-bold w-100">
-       Codecats
+    <div>
+        
+      <div className="text-3xl text-center font-bold">
+        Codecats
       </div>
 
-      <div className="flex gap-3 flex-row justify-center my-3">
-        <div className="flex text-3xl text-center items-center font-bold w-100">
+      <div className="flex flex-col items-center gap-3 my-3">
+
+        <div className="text-3xl text-center font-bold w-100 min-h-50">
           <img src="" alt="" />
           <p>Milton: jag pluggar devops och har kodat i ungefär 3 år</p>
         </div>
-        <div className="flex text-3xl text-center items-center font-bold w-100">
+
+        <div className="text-3xl text-center font-bold w-100 min-h-50">
           <img src="" alt="" />
-          <p>Abiha heter jag, är 28 och gillar smink och att virka. Nu vill jag koda igen och pluggar Fullstackutvecklare JavaScript.</p>
+          <p>
+            Abiha heter jag, är 28 och gillar smink och att virka. Nu vill jag
+            koda igen och pluggar Fullstackutvecklare JavaScript.
+          </p>
         </div>
-        <div className="flex text-3xl text-center items-center font-bold w-100">
+
+        <div className="text-3xl text-center font-bold w-100 min-h-50">
           <img src="" alt="" />
           <p>Alex: Jag pluggar till Frontendutvecklare med ai-kompetens</p>
         </div>
-        <div className="flex text-3xl text-center items-center font-bold w-100">
+
+        <div className="text-3xl text-center font-bold w-100 min-h-50">
           <img src="" alt="" />
           <p>Gillian: Jag pluggar .Net</p>
         </div>
       </div>
+
     </div>
   );
 }
