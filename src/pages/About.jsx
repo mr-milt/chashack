2babd47b754a9493c9dbb3b7bb940203268ae61f
mr-milt <milton@marjasin.se>
2026-09-11 11:37:14 +0200

    about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,30 @@
-import react from 'react'
+import react from "react";
 
-export default function About() { 
-    return (<div className=' text-red-500'>About</div>)
+export default function About() {
+  return (
+    <div className="flex flex-col justify-center text-red-500 gap-3 m-5">
+      <div className="flex justify-center text-3xl text-center items-center font-bold">
+        Om oss
+      </div>
+
+      <div className="flex gap-3 flex-row justify-center">
+        <div className="flex text-3xl text-center items-center font-bold">
+          <img src="" alt="" />
+          <p>Milton</p>
+        </div>
+        <div className="flex text-3xl text-center items-center font-bold">
+          <img src="" alt="" />
+          <p>Abiha</p>
+        </div>
+        <div className="flex text-3xl text-center items-center font-bold">
+          <img src="" alt="" />
+          <p>Alex</p>
+        </div>
+        <div className="flex text-3xl text-center items-center font-bold">
+          <img src="" alt="" />
+          <p>Gillian</p>
+        </div>
+      </div>
+    </div>
+  );
 }
-
